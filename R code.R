740c9354f362262d727b0c8668710f211ae0c45c
AnneChao <ljchao2012@gmail.com>
2023-08-25 02:37:31 +0800

    Creat the project.

---
+++ R code.R
@@ -0,0 +1,283 @@
+# ========================================================================================================== #
+
+# Install and upload libraries ----
+
+library(abind)
+library(ggpubr)
+library(plotly)
+library(lmtest)
+library(readxl)
+library(ggplot2)
+library(openxlsx)
+library(magrittr)
+library(parallel)
+library(reshape2)
+library(gridExtra)
+library(tidyverse)
+library(geosphere)
+library(patchwork)
+library(future.apply)
+
+
+# ========================================================================================================== #
+# 
+# This code includes five parts:
+# 
+# (1) Figure 2. iNEXT.beta3D standardization for beta diversity between Edge and Interior habitats for two rainforest fragments in Brazil. 
+# (2) Figure 3. Beta diversity between Edge and Interior habitats as a function of fragment size under six coverage values.
+# (3) Figure 4. The iNEXT.beta3D standardization for two time periods for beetle data.
+# (4) Figure 5. Trajectories of temporal beta diversity over time for tree species incidence data among 100 subplots in six second-growth rain forests.
+# (5) Figure 6. Temporal and spatial gamma, alpha and beta diversity for fish species based on SWC-IBTS data from 1985 to 2010.
+# 
+# See "Brief guide" for details. 
+# 
+# ========================================================================================================== #
+
+
+library(devtools)
+install_github("AnneChao/iNEXT.3D")        # Press 'Enter' to skip update options
+library(iNEXT.3D)
+install_github("AnneChao/iNEXT.beta3D")    # Press 'Enter' to skip update options
+library(iNEXT.beta3D)
+
+source("Source R code.txt")
+
+
+
+# ========================================================================================================== #
+# Figure 2. iNEXT.beta3D standardization for beta diversity between Edge and Interior habitats for two rainforest fragments in Brazil.
+raw_data = read_xlsx("Data rainforest trees.xlsx", sheet = 1)
+
+data_for_beta = lapply(1:12, function(i) {
+  tmp = raw_data[c(i, i+12), -(1:4)] %>% t
+  colnames(tmp) = c('Edge', 'Interior')
+  tmp
+  })
+names(data_for_beta) = raw_data$Site[1:12]
+
+
+## Figure 2 (a)
+output_fig_2a = iNEXTbeta3D(data_for_beta[c('Marim', 'Rebio 2')], diversity = 'TD', nboot = 200, base = 'size')
+fig_2a_or_4a(output_fig_2a)
+
+
+## Figure 2 (b)
+output_fig_2b = iNEXTbeta3D(data_for_beta[c('Marim', 'Rebio 2')], diversity = 'TD', nboot = 200, base = 'coverage')
+fig_2b_or_4b(output_fig_2b)
+
+
+
+# ========================================================================================================== #
+# Figure 3. Beta diversity between Edge and Interior habitats as a function of fragment size under six coverage values.
+output_fig_3 = iNEXTbeta3D(data_for_beta, "TD", q = c(0,1,2), nboot = 0, 
+                           level = c(0.6, 0.7, 0.8, 0.9, 0.95, 1))
+fig_3(output_fig_3)
+
+
+
+# ========================================================================================================== #
+# Figure 4. The iNEXT.beta3D standardization for two time periods for beetle data.
+
+## Figure 4 (a)
+beetle = list('Logged'   = read.xlsx('Data beetles.xlsx', rowNames = T, sheet = 1),
+              'Unlogged' = read.xlsx('Data beetles.xlsx', rowNames = T, sheet = 2))
+
+output_fig_4a = iNEXTbeta3D(beetle, datatype = 'abundance', base = "size", nboot = 200)
+fig_2a_or_4a(output_fig_4a)
+
+
+## Figure 4 (b)
+output_fig_4b = iNEXTbeta3D(beetle, datatype = 'abundance', base = 'coverage', nboot = 200,level = seq(0.8, 1, 0.025))
+fig_2b_or_4b(output_fig_4b)
+
+
+
+# ========================================================================================================== #
+# Figure 5. Trajectories of temporal beta diversity over time for tree species incidence data among 100 subplots in six second-growth rain forests.
+
+Cuat.raw   = read.xlsx("Data second-growth trees.xlsx", sheet = 1)
+LindEl.raw = read.xlsx("Data second-growth trees.xlsx", sheet = 2)
+Tiri.raw   = read.xlsx("Data second-growth trees.xlsx", sheet = 3)
+LindSu.raw = read.xlsx("Data second-growth trees.xlsx", sheet = 4)
+FEB.raw    = read.xlsx("Data second-growth trees.xlsx", sheet = 5)
+JE.raw     = read.xlsx("Data second-growth trees.xlsx", sheet = 6)
+
+
+inci.raw = list(SGF.data.transf(Cuat.raw   %>% filter(Year >= 2005)), 
+                SGF.data.transf(LindEl.raw %>% filter(Year >= 2005)), 
+                SGF.data.transf(Tiri.raw   %>% filter(Year >= 2005)), 
+                SGF.data.transf(LindSu.raw %>% filter(Year >= 2005)), 
+                SGF.data.transf(FEB.raw), 
+                SGF.data.transf(JE.raw))
+
+age = data.frame(Assem = c("Cuatro Rios", "Lindero el Peje", "Tirimbina", "Lindero Sur", "Finca el Bejuco", "Juan Enriquez"),
+                 Age = c(25,20,15,12,2,2))   ## fix on year 1997
+
+
+cpu.cores <- detectCores() - 1
+cl <- makeCluster(cpu.cores)
+clusterExport(cl, varlist = c("inci.raw", "for_fig_5", "obsbeta.incidence"), envir = environment())
+clusterEvalQ(cl, c(library(tidyverse), library(iNEXT.beta3D), library(iNEXT.3D), library(reshape2), 
+                   library(magrittr), library(abind), library(future.apply)))
+
+forests.output = parLapply(cl, inci.raw, function(x) for_fig_5(x, nboot = 200))
+
+stopCluster(cl)
+
+fig_5(forests.output)
+
+
+
+# ========================================================================================================== #
+# Figure 6. Temporal and spatial gamma, alpha and beta diversity for fish species based on SWC-IBTS data from 1985 to 2010.
+
+fish <- read.csv("Data fish_Lat55-60.csv")
+fish = fish %>% mutate(region = ifelse(LatBand %in% c(55.5, 56, 56.5, 57), 'South', 
+                                       ifelse(!(LatBand %in% c(57.5, 60)), 'North', 'Other'))) %>% filter(region != 'Other')
+groupyear = matrix(1985:2010, nrow = 2)
+colnames(groupyear) = paste(groupyear[1,], groupyear[2,], sep = '~')
+
+
+cpu.cores <- detectCores()-1
+cl <- makeCluster(cpu.cores)
+clusterExport(cl, varlist = c("rarefysamples", "fish", "groupyear"), envir = environment())
+clusterEvalQ(cl, c(library(tidyverse), library(iNEXT.beta3D), library(reshape2)))
+
+simu_output = parLapply(cl, 1:200, function(k) {
+  region <- unique(fish$region)
+  
+  TSrf <- list()
+  
+  for(i in 1:length(region)){
+    data2 <- fish[fish$region == region[i],]
+    TSrf[[i]] <- rarefysamples(data2) 
+  }
+  names(TSrf) <- region
+  
+  rf <- do.call(rbind, TSrf)
+  rf <- data.frame(rf, LatBand = rep(names(TSrf), times = unlist(lapply(TSrf, nrow))))
+  rf <- rf[!is.na(rf$Year), ]
+  rownames(rf)<-NULL
+  data = rf
+  
+  cov = c(0.99, 0.999, 1)
+  
+  ## ================== Temporal ================== ##
+  beta.temp = lapply(region, function(i) {
+    tmp = data %>% filter(LatBand %in% i)
+    
+    tmp2 = lapply(2:length(unique(tmp$Year)), function(j) {
+      g1 = dcast(tmp %>% filter(Year == sort(unique(tmp$Year))[1]), Species ~ LatBand, value.var = 'Abundance')
+      
+      g2 = dcast(tmp %>% filter(Year == sort(unique(tmp$Year))[j]), Species ~ LatBand, value.var = 'Abundance')
+      
+      out = full_join(g1, g2, by = 'Species')[,-1]
+      
+      out[is.na(out)] = 0
+      out
+    })
+    names(tmp2) = sort(unique(tmp$Year))[-1]
+    
+    return(tmp2)
+  })
+  
+  names(beta.temp) = region
+  
+  output.temp = lapply(1:length(beta.temp),  function(i) {
+    
+    result = iNEXTbeta3D(beta.temp[[i]], q = c(0, 1, 2), datatype = 'abundance', level = cov, nboot = 0)
+    
+    cbind(lapply(result, function(y) {
+      
+      tmp = lapply(1:3, function(i) cbind(y[[i]], div_type = names(y)[i])) %>% 
+        do.call(rbind,.) %>% filter(SC %in% cov | Method == 'Observed')
+      
+      obs = cbind(y$alpha, div_type = "beta") %>% filter(Method == 'Observed') %>%
+        mutate(Estimate = (y$gamma %>% filter(Method == 'Observed'))$Estimate / (y$alpha %>% filter(Method == 'Observed'))$Estimate, 
+               SC = "NA")
+      
+      rbind(tmp, obs)
+      
+    }) %>% do.call(rbind,.),
+    
+    Latitude = names(beta.temp)[i])
+  }) %>% do.call(rbind,.)
+  
+  output.temp$Dataset = as.numeric(output.temp$Dataset)
+  output.temp[output.temp$Method == 'Observed', 'SC'] = 'Observed'
+  
+  ## ================== Spatial ================== ##
+  beta.spat = lapply( list( c('South', 'North') ), function(i) {
+    tmp1 = data %>% filter(LatBand %in% i[[1]])
+    tmp2 = data %>% filter(LatBand %in% i[[2]])
+    
+    year = unique(data$Year) %>% sort
+    
+    tmp = lapply(year, function(j) {
+      g1 = dcast(tmp1 %>% filter(Year == j), Species ~ LatBand, value.var = 'Abundance')
+      g2 = dcast(tmp2 %>% filter(Year == j), Species ~ LatBand, value.var = 'Abundance')
+      
+      out = full_join(g1, g2, by = 'Species')[,-1]
+      
+      out[is.na(out)] = 0
+      out
+    })
+    names(tmp) = year
+    
+    return(tmp)
+  })
+  
+  names(beta.spat) = 'South vs. North'
+  
+  output.spat = lapply(1:length(beta.spat),  function(i) {
+    
+    result = iNEXTbeta3D(beta.spat[[i]], q = c(0, 1, 2), datatype = 'abundance', level = cov, nboot = 0)
+    
+    cbind(lapply(result, function(y) {
+      
+      tmp = lapply(1:3, function(i) cbind(y[[i]], div_type = names(y)[i])) %>% 
+        do.call(rbind,.) %>% filter(SC %in% cov | Method == 'Observed')
+      
+      obs = cbind(y$alpha, div_type = "beta") %>% filter(Method == 'Observed') %>%
+        mutate(Estimate = (y$gamma %>% filter(Method == 'Observed'))$Estimate / (y$alpha %>% filter(Method == 'Observed'))$Estimate, 
+               SC = "NA")
+      
+      rbind(tmp, obs)
+      
+    }) %>% do.call(rbind,.),
+    
+    Latitude = names(beta.temp)[i])
+  }) %>% do.call(rbind,.)
+  
+  output.spat$Dataset = as.numeric(output.spat$Dataset)
+  output.spat[output.spat$Method == 'Observed', 'SC'] = 'Observed'
+  
+  list("temporal" = output.temp[,c('Order.q','SC','Dataset','div_type','Latitude','Estimate')], 
+       "spatial" = output.spat[,c('Order.q','SC','Dataset','div_type','Latitude','Estimate')])
+})
+
+stopCluster(cl)
+
+
+output_fig_6 = list('temporal' = simu_output[[1]]$temporal,
+                    'spatial'  = simu_output[[1]]$spatial)
+
+for (i in 2:length(simu_output)) {
+  if (sum(simu_output[[i]]$temporal == 'Inf') == 0)
+    output_fig_6$temporal = full_join(output_fig_6$temporal, 
+                                      simu_output[[i]]$temporal, 
+                                      by = c('Order.q', 'SC', 'Dataset', 'div_type', 'Latitude'))
+  
+  if (sum(simu_output[[i]]$spatial == 'Inf') == 0)
+    output_fig_6$spatial = full_join(output_fig_6$spatial, 
+                                     simu_output[[i]]$spatial, 
+                                     by = c('Order.q', 'SC', 'Dataset', 'div_type', 'Latitude'))
+}
+
+output_fig_6$temporal = cbind(output_fig_6$temporal[,1:5], 'Estimate' = apply(output_fig_6$temporal[,-(1:5)], 1, mean))
+output_fig_6$spatial  = cbind(output_fig_6$spatial[,1:5],  'Estimate' = apply(output_fig_6$spatial[,-(1:5)],  1, mean))
+
+fig_6a(output_fig_6)
+fig_6b(output_fig_6)
+
+
---
+++ README.md
@@ -0,0 +1,25 @@
+ECM_iNEXT.beta3D (R code and data for graphics in 2023 Ecological Monographs paper by Chao et al.)
+
+R code and data are provided for plotting all figures in the paper "Rarefaction and extrapolation with beta diversity under a framework of Hill numbers: the iNEXT.beta3D standardization". Before using the R code, you must download the package "iNEXT.3D" and "iNEXT.beta3D" from Anne Chao's Github. 
+
+The data used for examples are based on (i) Spatial data: tropical tree species data collected from eight rainforest fragments in Brazil; (ii) Temporal data: beetle data collected in Germany for two time periods, and tree species long time series data (2005-2017) for 6 second-growth forests in Costa Rica; and (iii) Spatio-temporal data: Scottish fish species data (1985-2010).  
+
+"ECM_iNEXT.beta3D" includes the following files:
+
+(1) Data Files: 
+
+  (1a) "Data rainforest trees.xlsx" (for Figure 2 and 3): tree species data collected from eight rainforest fragments in Brazil.
+  
+  (1b) "Data beetles.xlsx" (for Figure 4): beetle data for two time periods (2008-2009 vs. 2010-2011).
+  
+  (1c) "Data second-growth trees.xlsx" (for Figure 5): tree species data in six second-growth forests from 2005-2017.
+  
+  (1d) "Data fish_Lat55-60.csv" (for Figure 6): Scottish fish data between latitude 55 and 60 from 1985-2010.
+
+(2) "R code.R": Main code for plotting all figures (Figure 2 to 6 in the paper).
+
+(3) "Source R code.txt": Source R code. 
+
+(4) "Brief guide iNEXT beta3D.docx": introduction to iNEXT.beta3D and a brief guide to R code for making graphics in Chao et al. 2023 paper.
+
+(5) "Environment.RData": output.
